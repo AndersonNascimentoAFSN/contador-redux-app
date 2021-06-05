9b0e688f733b8570bb184dc5cc67feb09c98b967
guilhermemd <guilherme.md@gmail.com>
2021-06-05 19:48:41 -0300

    vez da polly

--- src/Redux/reducers/counterReduce.js
+++ src/Redux/reducers/counterReduce.js
@@ -1,13 +1,20 @@
+import { ENCREMENTA, DECREMENTA } from '../actions/index'
 const INITIAL_STATE = {
-
+    numero: 0,
 }
 
 
 const counterReduce = (state = INITIAL_STATE, action) => {
-
+    
     switch(action.type) {
-    
+        case ENCREMENTA:
+            return {...state, numero: state.numero + 1 };
 
-    }
-    
+        case DECREMENTA:
+            return {...state, numero: state.numero - 1 };  
+        default:
+            return state;     
+    };  
 }
+
+export default counterReduce;
